748e818ac144521957269cc4c7ba279e643453b6
safwan hassan <safwan.hassan.d@gmail.com>
2023-06-10 17:45:35 +0500

    complete auth routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 
 const authController = require('./controllers/authController.js')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.post('/signup', authController.signup)
--- controllers/authController.js
+++ controllers/authController.js
@@ -3,15 +3,17 @@ const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
+  const user = await User.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
 
+  if (!user) return next(new AppError('User already exist', 401))
+
   res.status(401).json({
     status: 'success',
-    data: { newUser },
+    data: { user },
   });
 });
 
